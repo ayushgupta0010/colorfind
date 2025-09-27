dfcd5867633e95084ba1e2c7647cd3d584af1719
Nathan Cherny <84670821+Nathan-Cherny@users.noreply.github.com>
2025-09-27 17:26:28 -0400

    Update flask_app.py

--- website/flask_app.py
+++ website/flask_app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from werkzeug.utils import secure_filename
+from daltonize import daltonize
 import os
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = os.path.join("website", "static")
@@ -12,8 +15,20 @@ def main():
 @app.route("/uploads", methods=["POST"])
 def upload():
     image = request.files["my_image"]
-    imageSplit = image.filename.split(".")
     cb_type = request.form["colorblindnessType"]
-    image.save(os.path.join(app.config["UPLOAD_FOLDER"], secure_filename(f"{imageSplit[0]}_{cb_type}.{imageSplit[1]}")))
+    
+    imageSplit = image.filename.split(".")
+    newFile = secure_filename(f"{imageSplit[0]}_{cb_type}.{imageSplit[1]}")
+    
+    img = Image.open(image).convert("RGB")
+    img_arr = np.asarray(img, dtype=np.float16)
+    img_arr = daltonize.gamma_correction(img_arr)
+
+    dalton_fig = daltonize.daltonize(img_arr, color_deficit=cb_type[0].lower())
+    
+    img = daltonize.array_to_img(dalton_fig)
+    img.save(f"website/static/{newFile}")
+    
+    return url_for("static", filename=newFile)
 
 app.run(debug=True)
